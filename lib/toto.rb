ee03afb93b0d2fcf70225f677bc7251d74d09076
nviarnes <arcrutus@gmail.com>
2012-03-01 11:40:30 -0800

    woohoo

--- lib/toto.rb
+++ lib/toto.rb
@@ -78,15 +78,46 @@ module Toto
       end}.merge archives
     end
 
-    def archives filter = ""
-      entries = ! self.articles.empty??
-        self.articles.select do |a|
-          filter !~ /^\d{4}/ || File.basename(a) =~ /^#{filter}/
-        end.reverse.map do |article|
-          Article.new article, @config
-        end : []
+    def go route, env = {}, type = :html 
+      route << self./ if route.empty?
+      type, path = type =~ /html|xml|json/ ? type.to_sym : :html, route.join('/')
+      context = lambda do |data, page|
+        Context.new(data, @config, path, env).render(page, type)
+      end 
 
-      return :archives => Archives.new(entries, @config)
+      body, status = if Context.new.respond_to?(:"to_#{type}")
+        if route.first =~ /\d{4}/ 
+          case route.size
+            when 1..3 
+              context[archives(route * '-'), :archives]
+            when 4 
+              context[article(route), :article]
+            else http 400 
+          end 
+
+        elsif route.first == 'tags' && route.size == 2 
+          if (data = archives('', route[1])).nil?
+            http 404 
+          else 
+            context[data, :tag]
+          end
+
+        elsif respond_to?(path)
+          context[send(path, type), path.to_sym]
+        elsif (repo = @config[:github][:repos].grep(/#{path}/).first) &&
+              !@config[:github][:user].empty?
+          context[Repo.new(repo, @config), :repo]
+        else 
+          context[{}, path.to_sym]
+        end 
+      else 
+        http 400 
+      end 
+
+    rescue Errno::ENOENT => e
+      return :body => http(404).first, :type => :html, :status => 404 
+    else 
+      return :body => body || "", :type => type, :status => status || 200 
     end
 
     def article route
@@ -97,38 +128,46 @@ module Toto
       self[:root]
     end
 
-    def go route, env = {}, type = :html
+    def go route, env = {}, type = :html 
       route << self./ if route.empty?
       type, path = type =~ /html|xml|json/ ? type.to_sym : :html, route.join('/')
       context = lambda do |data, page|
         Context.new(data, @config, path, env).render(page, type)
-      end
+      end 
 
       body, status = if Context.new.respond_to?(:"to_#{type}")
-        if route.first =~ /\d{4}/
+        if route.first =~ /\d{4}/ 
           case route.size
-            when 1..3
+            when 1..3 
               context[archives(route * '-'), :archives]
-            when 4
+            when 4 
               context[article(route), :article]
-            else http 400
+            else http 400 
+          end 
+
+        elsif route.first == 'tags' && route.size == 2 
+          if (data = archives('', route[1])).nil?
+            http 404 
+          else 
+            context[data, :tag]
           end
+
         elsif respond_to?(path)
           context[send(path, type), path.to_sym]
         elsif (repo = @config[:github][:repos].grep(/#{path}/).first) &&
               !@config[:github][:user].empty?
           context[Repo.new(repo, @config), :repo]
-        else
+        else 
           context[{}, path.to_sym]
-        end
-      else
-        http 400
-      end
+        end 
+      else 
+        http 400 
+      end 
 
     rescue Errno::ENOENT => e
-      return :body => http(404).first, :type => :html, :status => 404
-    else
-      return :body => body || "", :type => type, :status => status || 200
+      return :body => http(404).first, :type => :html, :status => 404 
+    else 
+      return :body => body || "", :type => type, :status => status || 200 
     end
 
   protected
